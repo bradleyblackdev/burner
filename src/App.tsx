99c6e55258d241d7d07a69dbb7659a11f0f2b2a8
Bradley Black <bradleyblackdev@gmail.com>
2023-08-23 13:34:22 -0500

    sdfds

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
-  // Route,
-  // Routes,
+  Route,
+  Routes,
 //   Link,
 //   useParams
 } from "react-router-dom";
@@ -12,24 +12,24 @@ import {
 function App() {
   return (
     <Router>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="complete" element={<Users />} />
+    </Routes>
+  </Router>
   );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+  }
+  
+  // function Complete() {
+  // return (<Link to="/">reset</Link>)
+  // }
+  
+  function Users() {
+  return <h2>Users</h2>;
+  }
 
 export default App;
